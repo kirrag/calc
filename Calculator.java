bc18f32c777b2270ae8d3a7211f8b1e0ba564249
Garrik Ten <ten@otux.ru>
2023-02-02 12:17:42 +0300

    moved block try-catch from Main to Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,8 @@
-import java.util.function.*;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Calculator {
 	static Supplier<Calculator> instance = Calculator::new;
@@ -6,7 +10,16 @@ public class Calculator {
 	BinaryOperator<Integer> plus = (x, y) -> x + y;
 	BinaryOperator<Integer> minus = (x, y) -> x - y;
 	BinaryOperator<Integer> multipy = (x, y) -> x * y;
-	BinaryOperator<Integer> devide = (x, y) -> x / y;
+	BinaryOperator<Integer> devide = (x, y) -> {
+		try {
+			return x / y;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	};
+	
+
+	//BinaryOperator<Integer> devide = (x, y) -> x / y;
 	UnaryOperator<Integer> pow = x -> x * x;
 	UnaryOperator<Integer> abs = x -> x > 0 ? x: x * -1;
 
@@ -14,3 +27,4 @@ public class Calculator {
 
 	Consumer<Integer> println = System.out::println;
 }
+
--- Main.java
+++ Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 		int a = calc.plus.apply(1, 2);
 		int b = calc.minus.apply(1, 1);
-		try {
-			int c = calc.devide.apply(a, b);
-			calc.println.accept(c);
-		} catch (ArithmeticException e) {
-			System.out.println("Ошибка: Деление на ноль: " + e);
-		}
+		int c = calc.devide.apply(a, b);
+		calc.println.accept(c);
 	}
 }
